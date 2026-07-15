b0b9ab59c225853c9409f73c658ceb96e778cfce
Jim Myhrberg <contact@jimeh.me>
2026-07-15 13:54:37 +0100

    feat: support hosted browser views

---
+++ src/vs/platform/browserView/electron-main/browserViewHostedWebContents.ts
@@ -0,0 +1,56 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import type { BrowserView } from './browserView.js';
+
+/**
+ * Tracks integrated browser views owned by hosted Omni workbench webContents.
+ */
+export class BrowserViewHostedWebContents {
+	private readonly visibility = new Map<number, boolean>();
+
+	isVisible(hostedWebContentsId: number | undefined): boolean {
+		if (typeof hostedWebContentsId !== 'number') {
+			return true;
+		}
+
+		return this.visibility.get(hostedWebContentsId) ?? true;
+	}
+
+	setVisible(
+		hostedWebContentsId: number,
+		visible: boolean,
+		views: Iterable<BrowserView>
+	): void {
+		this.visibility.set(hostedWebContentsId, visible);
+		for (const view of views) {
+			view.setHostedWebContentsVisible(hostedWebContentsId, visible);
+		}
+	}
+
+	bringToFront(
+		hostedWebContentsId: number,
+		views: Iterable<BrowserView>
+	): void {
+		for (const view of views) {
+			view.bringToFrontForHostedWebContents(hostedWebContentsId);
+		}
+	}
+
+	delete(hostedWebContentsId: number): void {
+		this.visibility.delete(hostedWebContentsId);
+	}
+
+	getOwnedViewIds(
+		hostedWebContentsId: number,
+		views: Iterable<[string, BrowserView]>
+	): string[] {
+		return Array.from(views)
+			.filter(([, view]) =>
+				view.belongsToHostedWebContents(hostedWebContentsId)
+			)
+			.map(([id]) => id);
+	}
+}
