b8e6c9f7aa7ea2d4c049665be0f65d18c3b42d4e
Jim Myhrberg <contact@jimeh.me>
2026-08-02 01:35:18 +0000

    feat(web): select serve-web user data storage

--- src/vs/server/node/serverEnvironmentService.ts
+++ src/vs/server/node/serverEnvironmentService.ts
@@ -32,6 +32,7 @@ export const serverOptions: OptionDescriptions<Required<ServerParsedArgs>> = {
 	'accept-server-license-terms': { type: 'boolean', cat: 'o', description: nls.localize('acceptLicenseTerms', "If set, the user accepts the server license terms and the server will be started without a user prompt.") },
 	'server-data-dir': { type: 'string', cat: 'o', description: nls.localize('serverDataDir', "Specifies the directory that server data is kept in.") },
 	'hucode-web-omni-root': { type: 'boolean' },
+	'hucode-web-user-data-storage': { type: 'string' },
 	'telemetry-level': { type: 'string', cat: 'o', args: 'level', description: nls.localize('telemetry-level', "Sets the initial telemetry level. Valid levels are: 'off', 'crash', 'error' and 'all'. If not specified, the server will send telemetry until a client connects, it will then use the clients telemetry setting. Setting this to 'off' is equivalent to --disable-telemetry") },
 
 	/* ----- vs code options ---	-- */
@@ -162,6 +163,7 @@ export interface ServerParsedArgs {
 	'server-data-dir'?: string;
 
 	'hucode-web-omni-root'?: boolean;
+	'hucode-web-user-data-storage'?: 'browser' | 'server';
 
 	'telemetry-level'?: string;
 
